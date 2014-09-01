4cb337b2f3e291c2f8cf582ca49c7c2a74444d13
enoki <enoki@tagen.tohoku.ac.jp>
2014-09-01 13:29:40 +0900

    read nan

--- bin/rm_checkrx.py
+++ bin/rm_checkrx.py
@@ -5,9 +5,10 @@ atat/checkrelaxの結果を受け取って
 歪みが大きい構造(0.1以上)を除外する
 歪みが大きい構造dirにerrorファイルを置く
 """
-import math
 import os
 from subprocess import Popen, PIPE
+
+__date__ = "Sep 1 2014"
 
 def main():
     """main"""
@@ -17,17 +18,20 @@ def touch_errors():
     """
     構造歪みの大きい(0.1以上)のdirにtouch errorする
     """
-    checkrelax = Popen('checkrelax', stdout=PIPE).communicate()[0]
-    distortion = [float(x.split()[0])
-                  for x in checkrelax.split('\n')[:-1]]
-    print(distortion.count(float('nan')))
+    checkrelax = Popen('checkrelax',
+                       stdout=PIPE).communicate()[0].split('\n')[:-1]
 
+    vals = [x for x in checkrelax if x.split()[0] != 'nan']
+    nons = [x for x in checkrelax if x.split()[0] == 'nan']
+    checkrelax = vals + nons
+
+    distortion = [float(x.split()[0]) for x in checkrelax]
 
     # checkrelaxの値が0.1以上になる行を判定
     threshold = 0
     while distortion[threshold] <= 0.1:
         threshold += 1
-    distorted_list = checkrelax.split('\n')[threshold:-1]
+    distorted_list = checkrelax[threshold:]
     distorted = '\n'.join(distorted_list)
     with open('listDistorted.txt', 'w') as wfile:
         wfile.write(distorted)
